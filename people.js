1f903b0fc0f6781436cf223a95e4c6656ace419c
Orsouene <orsoueneelaouizeb@gmail.com>
2024-11-29 16:37:17 +0100

    Prima letta del nome e del cognome Maj

--- people.js
+++ people.js
@@ -9,7 +9,12 @@ function people() {
   const utente = allNames.fullNames("orsouene", "elaouizeb");
   return {
     // Nome concatinato
-    fullName: utente.firstName + " " + utente.lastName,
+    fullName:
+      utente.firstName.charAt(0).toUpperCase() +
+      utente.firstName.slice(1) +
+      " " +
+      utente.lastName.charAt(0).toUpperCase() +
+      utente.lastName.slice(1),
     // l'array di hobbies
     hobbies: allHobbies.hobbiesFun("football", "gaming", "coding").hobbies,
   };
