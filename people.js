0e52f782b0931b3ee904885a34157bdad3028603
Orsouene <orsoueneelaouizeb@gmail.com>
2024-11-29 16:29:41 +0100

    3-Creato una funzione per people

---
+++ people.js
@@ -0,0 +1,18 @@
+// import from names.js
+const allNames = require("./names");
+// import from hobbies.js
+const allHobbies = require("./hobbies");
+
+// function people
+function people() {
+  //  salvo l'oggetto fullnames
+  const utente = allNames.fullNames("orsouene", "elaouizeb");
+  return {
+    // Nome concatinato
+    fullName: utente.firstName + " " + utente.lastName,
+    // l'array di hobbies
+    hobbies: allHobbies.hobbiesFun("football", "gaming", "coding").hobbies,
+  };
+}
+
+console.log(people());
